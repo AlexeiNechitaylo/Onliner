976715c43a32ec49ee6372809539ad917f8ae6a1
AlexeiNechitaylo <nechytaila89@gmail.com>
2019-08-30 00:31:17 +0300

    First commit

--- onliner.py
+++ onliner.py
@@ -17,31 +17,26 @@ class Test_onliner(unittest.TestCase):
 	def login(self, wd):
 		self.open_page(wd)
 		wd.maximize_window()
+		wd.find_element_by_css_selector("div.auth-bar div div.auth-bar").click() #click entry button
 		wd.find_element_by_xpath("//input[@placeholder='Ник или e-mail']").clear()
 		wd.find_element_by_xpath("//input[@placeholder='Ник или e-mail']").send_keys("ravlikgicel@gmail.com")
 		wd.find_element_by_xpath("//input[@placeholder='Пароль']").clear()
 		wd.find_element_by_xpath("//input[@placeholder='Пароль']").send_keys("6qwe1qwe")
 		wd.find_element_by_css_selector("div.auth-form button.auth-button").click()
+		
 
-	def entry_button(self, wd):
-		wd.find_element_by_css_selector("div.auth-bar div div.auth-bar").click()
-
-
-	def find_goods(self, wd):
-		wd.
-
-
-	def add_to_cart(self, wd):
-		wd.
-
-
+	def find_goods_and_add_to_cart(self, wd):
+		wd.find_element_by_css_selector("input.fast-search__input").clear()
+		wd.find_element_by_css_selector("input.fast-search__input").send_keys("духовая печь")
+		
+		
+		
+	def tearDown(self):
+		self.wd.quit()
+'''
 	def click_cart(self, wd):
 		cart_icon = WebDriverWait(wd, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div#cart-desktop a.b-top-profile__cart")))
 		cart_icon.click()
-
-	'''
-	verifying
-	'''
 
 
 	def click_onliner_header(self, wd):
@@ -62,10 +57,9 @@ class Test_onliner(unittest.TestCase):
 		self.add_to_cart(wd)
 		self.click_onliner_header(wd)
 		self.logout(wd)
+'''
 
 
-	def tearDown(self):
-		self.wd.quit()
 
 
 if __name__== '__main__':
