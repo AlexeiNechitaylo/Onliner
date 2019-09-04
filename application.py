d568c37dc7a1e506f57bdd22cd4723d83db97d20
AlexeiNechitaylo <nechytaila89@gmail.com>
2019-09-04 21:43:44 +0300

    fixture

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class App:
+class Application:
+
     def __init__(self):
         self.wd = webdriver.Chrome()
 
--- onliner.py
+++ onliner.py
@@ -1,16 +1,16 @@
 import pytest
 from user_data import User_data
-from app import App
+from application import Application
 
 
-@pytest.fixture()
+@pytest.fixture
 def app(request):
-	fixture = App()
+	fixture = Application()
 	request.addfinalizer(fixture.destroy)
 	return fixture
 
 
-def test_onliner(app):
+def test_onl(app):
 	app.login(User_data(email="ravlikgicel@gmail.com", password="6qwe1qwe"))
 	app.click_cart()
 	app.click_onliner_header()
